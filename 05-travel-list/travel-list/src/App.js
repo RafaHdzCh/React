a2e1d37cbc7b2d7cb3b239c0128301a2bc6f3455
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-06 12:41:07 -0600

    Far Away - Sorting items

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -92,11 +92,18 @@ function Form({OnAddItems})
 
 function PackingList({items, OnDeleteItem, OnToggleItem})
 {
+  const [sortBy, SetSortBy] = useState("input");
+
+  let sortedItems;
+  if(sortBy === "input") sortedItems=items;
+  if(sortBy === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+  if(sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+ 
   return (
   <div className="list">
     <ul>
       {
-        items.map(item => 
+        sortedItems.map(item => 
         <Item 
           item={item} 
           OnDeleteItem={OnDeleteItem}
@@ -104,6 +111,18 @@ function PackingList({items, OnDeleteItem, OnToggleItem})
           key={item.id}/>
       )}
     </ul>
+
+    <div className="actions">
+      <select value={sortBy} onChange={event => SetSortBy(event.target.value)}>
+        <option value="input"> Sort by input order </option>
+        <option value="description"> Sort by description </option>
+        <option value="packed"> Sort by packed status </option>
+      </select>
+
+      <button>
+        Clear list
+      </button>
+    </div>
   </div>
   )
 }
