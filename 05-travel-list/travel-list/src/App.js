61e2f5a6bbce06867bdd2122e4730caa8492d2ef
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-06 12:46:54 -0600

    Far Away - Clear list

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -21,6 +21,12 @@ export default function App()
     SetItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed} : item));
   }
 
+  function HandleClearList()
+  {
+    const confirmed = window.confirm("Are you sure you want to delete all items?")
+    if(confirmed) SetItems([]);
+  }
+
   //#endregion
   
   return(
@@ -33,6 +39,7 @@ export default function App()
       items={items} 
       OnDeleteItem={HandleDeleteItem} 
       OnToggleItem={HandleToggleItem}
+      OnClearList={HandleClearList}
     />
     <Stats 
       items={items} 
@@ -90,7 +97,7 @@ function Form({OnAddItems})
   )
 }
 
-function PackingList({items, OnDeleteItem, OnToggleItem})
+function PackingList({items, OnDeleteItem, OnToggleItem, OnClearList})
 {
   const [sortBy, SetSortBy] = useState("input");
 
@@ -119,7 +126,7 @@ function PackingList({items, OnDeleteItem, OnToggleItem})
         <option value="packed"> Sort by packed status </option>
       </select>
 
-      <button>
+      <button onClick={OnClearList}>
         Clear list
       </button>
     </div>
