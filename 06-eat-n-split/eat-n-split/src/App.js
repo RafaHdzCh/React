a7420b2049c6344d8dca5b6ad80add9100ad2d2a
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-09 12:51:09 -0600

    Eat n Split - Key props

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -76,6 +76,7 @@ export default function App()
       <FormSplitBill 
         selectedFriend={selectedFriend} 
         OnSplitBill={HandleSplitBill}
+        key={selectedFriend.id}
       />}
     </div>
   )
