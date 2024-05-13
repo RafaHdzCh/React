397c2f83d995997e3904d22d2f90b0bf39436f86
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-13 08:19:02 -0600

    UsePopcorn - Synchronizing queries with movie data

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -1,64 +1,32 @@
 import { useEffect, useState } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const API_KEY = "f8dc2545";
-
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() 
 {
+  const [query, SetQuery] = useState("");
   const [movies, SetMovies] = useState([]);
   const [watched, SetWatched] = useState([]);
   const [isLoading, SetIsLoading] = useState(false);
   const [error, SetError] = useState("");
-  const query = "interstellar";
 
+/*
+  useEffect(function()
+  {
+    console.log("After initial render")
+  }, [])
+  useEffect(function()
+  {
+    console.log("After every render")
+  })
+  useEffect(function()
+  {
+    console.log("On query")
+  }, [query])
+  console.log("During render")
+*/
+  
   useEffect(function() 
   {
     async function FetchMovies()
@@ -66,6 +34,7 @@ export default function App()
       try
       {
         SetIsLoading(true);
+        SetError("");
         
         const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`)
         if(!response.ok) throw new Error("Something went wrong...")
@@ -77,7 +46,6 @@ export default function App()
       }
       catch(error)
       {
-        console.error(error.message);
         SetError(error.message)
       }
       finally
@@ -85,14 +53,20 @@ export default function App()
         SetIsLoading(false);
       }
     }
+    if(!query.length)
+    {
+      SetMovies([]);
+      SetError("");
+      return;
+    }
     FetchMovies();
-  }, []);
+  }, [query]);
 
   return (
     <>
       <NavBar>
         <Logo/>
-        <SearchBar />
+        <SearchBar query={query} SetQuery={SetQuery}/>
         <NumberOfResults movies={movies}/>
       </NavBar>
 
@@ -116,6 +90,7 @@ function Loader()
 {
   return <p className="loader"> Loading... </p>
 }
+
 function ErrorMessage({message})
 {
   return <p className="error"> <span> {message} </span> </p>
@@ -126,17 +101,15 @@ function NavBar({children})
   return <nav className="nav-bar"> {children}</nav>
 }
 
-function SearchBar()
+function SearchBar({query, SetQuery})
 {
-  const [query, setQuery] = useState("");
-  
   return(
     <input
         className="search"
         type="text"
         placeholder="Search movies..."
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => SetQuery(e.target.value)}
     />
   )
 }
@@ -155,7 +128,7 @@ function NumberOfResults({movies})
 {
   return(
     <p className="num-results">
-      Found <strong>{movies.length}</strong> results
+      Found <strong>{movies?.length}</strong> results
     </p>
   )
 }
