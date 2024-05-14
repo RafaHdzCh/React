1b71ee99c9c4362f8d0480f118f2febbbb48faa6
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-14 11:57:04 -0600

    ReactQuiz - Loading questions from a Fake API

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,8 +1,37 @@
-import Header from "./Header";
 import Main from "./Main";
+import Header from "./Header";
+import {useEffect, useReducer} from "react";
+
+const initialState = 
+{
+  questions: [],
+  //loading, error, ready, active, finish
+  status: "loading" 
+};
+
+function Reducer(state, Action)
+{
+  switch(Action.type)
+  {
+    case "DatFailed": return {...state, status: "error", }
+    case "DataReceived": return {...state, questions: Action.payload, status: "ready"};
+    
+    default: throw new Error("Action unknown.")
+  }
+}
 
 export default function App()
 {
+  const [state, Dispatch] = useReducer(Reducer,initialState);
+
+  useEffect(function()
+  {
+    fetch("http://localhost:8000/questions")
+      .then(response=>response.json())
+      .then(data=>Dispatch({type: "DataReceived", payload: data}))
+      .catch(error => Dispatch({type: "DataFailed"}));
+  });
+
   return (
     <div>
       <Header />
