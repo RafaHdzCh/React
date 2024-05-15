9457865bf7afa1b2d3c9190813ee89a8079c2ecd
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-15 12:31:02 -0600

    WorldWise - Using CSS modules

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,6 +1,7 @@
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -10,9 +11,10 @@ export default function App()
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage />}/>
+      <Route path="app" element={<AppLayout />}/>
       <Route path="product" element={<Product />}/>
       <Route path="pricing" element={<Pricing />}/>
-      
+
       <Route path="*" element={<PageNotFound />}/>
     </Routes>
   </BrowserRouter>);
--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom"
 import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
 
 export default function Homepage()
 {
   return (
     <> 
       <PageNav />
+      <AppNav />
       <h1> WorldWise</h1>
+
+      <Link to="/app">Go To The App</Link>
     </>)
 }
