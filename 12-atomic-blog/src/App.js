d07da997e3206727021e915c333a8822bba7f2b5
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-05-27 08:13:15 -0600

    AtomicBlog - Consuming the context

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -8,12 +8,15 @@ function createRandomPost() {
   };
 }
 
-function App() {
-  const [posts, setPosts] = useState(() =>
+//1) Create a context
+const PostContext = createContext();
+
+export default function App() {
+  const [posts, SetPosts] = useState(() =>
     Array.from({ length: 30 }, () => createRandomPost())
   );
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isFakeDark, setIsFakeDark] = useState(false);
+  const [searchQuery, SetSearchQuery] = useState("");
+  const [isFakeDark, SetIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
   const searchedPosts =
@@ -25,101 +28,122 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
+  function HandleAddPost(post) 
+  {
+    SetPosts((posts) => [post, ...posts]);
   }
 
-  function handleClearPosts() {
-    setPosts([]);
+  function HandleClearPosts() 
+  {
+    SetPosts([]);
   }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
-    function () {
+    function () 
+    {
       document.documentElement.classList.toggle("fake-dark-mode");
     },
     [isFakeDark]
   );
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
+    //2) Provide value to child components
+    <PostContext.Provider value=
+    {
+      {
+        posts:searchedPosts,
+        onAddPost:HandleAddPost,
+        onClearPosts:HandleClearPosts,
+        searchQuery:searchQuery,
+        setSearchQuery:SetSearchQuery
+      }
+    }>
+      <section>
+        <button
+          onClick={() => SetIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
 
-      <Header
-        posts={searchedPosts}
-        onClearPosts={handleClearPosts}
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-      <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive onAddPost={handleAddPost} />
-      <Footer />
-    </section>
+        <Header />
+        <Main />
+        <Archive onAddPost={HandleAddPost} />
+        <Footer />
+      </section>
+    </PostContext.Provider>
   );
 }
 
-function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+function Header() 
+{
+  const context = useContext(PostContext);
+
   return (
     <header>
       <h1>
         <span>⚛️</span>The Atomic Blog
       </h1>
       <div>
-        <Results posts={posts} />
-        <SearchPosts
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
-        <button onClick={onClearPosts}>Clear posts</button>
+        <Results />
+        <SearchPosts />
+        <button onClick={context.onClearPosts}>Clear posts</button>
       </div>
     </header>
   );
 }
 
-function SearchPosts({ searchQuery, setSearchQuery }) {
+function SearchPosts() 
+{
+  const context = useContext(PostContext);
+
   return (
     <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
+      value={context.searchQuery}
+      onChange={(event) => context.setSearchQuery(event.target.value)}
       placeholder="Search posts..."
     />
   );
 }
 
-function Results({ posts }) {
-  return <p>🚀 {posts.length} atomic posts found</p>;
+function Results() 
+{
+  const context = useContext(PostContext);
+
+  return <p>🚀 {context.posts.length} atomic posts found</p>;
 }
 
-function Main({ posts, onAddPost }) {
+function Main() 
+{
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts  />
     </main>
   );
 }
 
-function Posts({ posts }) {
+function Posts() 
+{
   return (
     <section>
-      <List posts={posts} />
+      <List />
     </section>
   );
 }
 
-function FormAddPost({ onAddPost }) {
+function FormAddPost() 
+{
+  const context = useContext(PostContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
-  const handleSubmit = function (e) {
-    e.preventDefault();
+  const handleSubmit = function (event) 
+  {
+    event.preventDefault();
     if (!body || !title) return;
-    onAddPost({ title, body });
+    context.onAddPost({ title, body });
     setTitle("");
     setBody("");
   };
@@ -141,20 +165,26 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
-function List({ posts }) {
+function List() 
+{
+  const context = useContext(PostContext);
   return (
     <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
+      {
+        context.posts.map((post, index) => (
+          <li key={index}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))
+      }
     </ul>
   );
 }
 
-function Archive({ onAddPost }) {
+function Archive() 
+{
+  const context = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -177,7 +207,7 @@ function Archive({ onAddPost }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
+              <button onClick={() => context.onAddPost(post)}>Add as new post</button>
             </li>
           ))}
         </ul>
@@ -186,8 +216,7 @@ function Archive({ onAddPost }) {
   );
 }
 
-function Footer() {
+function Footer() 
+{
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
 }
-
-export default App;
