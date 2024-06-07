8f28f187aac75510cc7d8602725c69d09ecb3fe5
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-07 15:43:12 -0600

    ReduxIntro - Creating a reducer

---
+++ 14-redux-intro/src/store.js
@@ -0,0 +1,18 @@
+const initialState = 
+{
+  balance: 0,
+  loan: 0,
+  loanPurpose: ""
+}
+
+function reducer(state = initialState, action) 
+{
+  switch(action.type)
+  {
+    case "account/deposit": return {...state, balance: state.balance + action.payload};
+    case "account/withdrwa": return {...state, balance: state.balance - action.payload};
+    case "account/requestLoan": return state.loan > 0 ? state : {...state, loan: action.payload};
+    case "account/payLoan": return {...state, loan:0, loanPurpose:"", balance:state.balance-state.loan}
+    default: return state;
+  }
+}
