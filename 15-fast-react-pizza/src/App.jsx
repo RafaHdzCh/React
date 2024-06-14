72c50c02d59bf2ee88dfabee4768a7e1556d0237
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-14 11:16:48 -0600

    FastPizza - Implementing routes

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,4 +1,20 @@
+import Home from "./ui/Home";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home /> },
+    { path: "/menu", element: <Menu /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/order/:orderId", element: <Order /> },
+    { path: "/order/new", element: <CreateOrder /> },
+  ])
+
 export default function App() 
 {
-  return <div> Hello vite</div>
+  return <RouterProvider router={router} />;
 }
