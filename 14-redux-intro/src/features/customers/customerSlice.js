537c5eadee6cdce473cf035b01f64c55ce4c45c8
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-14 09:30:26 -0600

    ReduxIntro - customerSlice with RTK

--- 14-redux-intro/src/features/customers/customerSlice.js
+++ 14-redux-intro/src/features/customers/customerSlice.js
@@ -1,10 +1,50 @@
-const initialStateCustomer =
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState =
 {
   fullName: "",
   nationalID: "",
   createdAt: "",
 }
 
+const customerSlice = createSlice(
+{
+  name: "customer",
+  initialState,
+  reducers:
+  {
+    CreateCustomer: 
+    {
+      prepare(fullName, nationalID)
+      {
+        return{
+          payload:
+          {
+            fullName, 
+            nationalID, 
+            createdAt: new Date().toISOString(),
+          }
+        }
+      },
+      reducer(state, action)
+      {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      }
+    },
+    UpdateName(state, action)
+    {
+      state.fullName = action.payload;
+    }
+  }
+});
+
+export const {CreateCustomer, UpdateName} = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 export default function CustomerReducer(state = initialStateCustomer, action)
 {
   switch(action.type)
@@ -44,3 +84,4 @@ export function UpdateName(fullName)
 {
   return {type: "customer/updateName", payload: fullName};
 }
+*/
