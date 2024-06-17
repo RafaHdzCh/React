def3efe0f0189ff47e2ff7a21215f95741f08f28
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-17 12:55:29 -0600

    FastPizza - Styling form elements

--- 15-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 15-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -19,7 +19,23 @@ export default function SearchOrder()
         placeholder="Search order #" 
         value={query} 
         onChange={event=>SetQuery(event.target.value)}
-      />
+        className="
+        w-28
+        bg-yellow-100
+        rounded-full 
+        px-4 
+        py-2 
+        text-sm 
+        placeholder:text-stone-400
+        sm:w-64
+        sm:focus:w-72
+        transition-all
+        duration-300
+        focus:outline-none
+        focus:ring
+        focus:ring-yellow-500
+        focus:ring-opacity-50
+      "/>
     </form>
   )
 }
