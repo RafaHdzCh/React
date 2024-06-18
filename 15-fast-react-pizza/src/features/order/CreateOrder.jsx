17623125a78ee68d88e6336118f37b731f6b602d
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-18 14:21:47 -0600

    FastPizza - Styling order form

--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -43,36 +43,48 @@ export default function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="text-xl font-semibold mb-8">Ready to order?</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input"/>
-        </div>
-
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input"/>
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <div className="grow">
             <input 
               type="text" 
-              name="address" 
+              name="customer" 
               required 
-              className="input"
+              className="input w-full"
             />
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input 
+              type="tel" 
+              name="phone"
+              required 
+              className="input w-full"
+            />
+            {formErrors?.phone && <p className="text-xs mt-2 text-red-700 bg-red-100 rounded-md p-2">{formErrors.phone}</p>}
+          </div>
+        </div>
+
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input 
+              type="text" 
+              name="address" 
+              required 
+              className="input w-full"
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex gap-5 items-center">
           <input
             type="checkbox"
             name="priority"
@@ -89,7 +101,7 @@ export default function CreateOrder() {
           // value={withPriority}
           // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">
+          <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
             </label>
         </div>
