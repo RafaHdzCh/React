292390966c24131effe24288e0a6ab8041ea5e10
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-18 09:51:23 -0600

    FastPizza Tailwind custom font

--- 15-fast-react-pizza/tailwind.config.js
+++ 15-fast-react-pizza/tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: 
+  {
+    fontFamily:
+    {
+      sans: "Roboto Mono, monospace"
+    },
     extend: {},
   },
   plugins: [],
