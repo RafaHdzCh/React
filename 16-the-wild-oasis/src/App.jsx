54a34de50c61b0159907dfda833bf73e1f04140f
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-20 15:32:16 -0600

    WildOasis - Displaying toasts

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -9,7 +9,8 @@ import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
@@ -30,6 +31,24 @@ export default function App()
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{
+          margin: "8px",
+        }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
@@ -46,6 +65,7 @@ export default function App()
           <Route path="*" element={<PageNotFound />}/>
         </Routes>
       </BrowserRouter>
+    
     </QueryClientProvider>
   )
 }
--- 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers"
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { DeleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -43,33 +44,36 @@ const Discount = styled.div`
 `;
 
 
-export default function CabinRow({cabin})
+export default function CabinRow({ cabin }) 
 {
-  const {id: cabinId, name, maxCapacity, regularPrice, discount, image}=cabin;
+  const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   const queryClient = useQueryClient();
 
-  const {isLoading: isDeleting, mutate} = useMutation(
+  const { isLoading: isDeleting, mutate } = useMutation(
   {
     mutationFn: DeleteCabin,
     onSuccess: () => 
     {
+      toast.success("Cabin successfully deleted!");
       queryClient.invalidateQueries(
       {
-        queryKey: ["cabin"]
-      })
+        queryKey: ["cabin"],
+      });
     },
-    onError: error => alert(error.message)
-  })
+    onError: (error) => toast.error(error.message),
+  });
 
-  return(
+  return (
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fit up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
-  )
+  );
 }
