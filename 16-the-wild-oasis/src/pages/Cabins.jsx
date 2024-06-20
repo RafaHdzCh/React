d7118d88de3691c6450c74f87c9521d112bfceaa
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-20 11:23:38 -0600

    WildOasis - Connecting Supabase with our react app

--- 16-the-wild-oasis/src/pages/Cabins.jsx
+++ 16-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import {GetCabins} from "../services/apiCabins.js"
 
 function Cabins() {
+
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
