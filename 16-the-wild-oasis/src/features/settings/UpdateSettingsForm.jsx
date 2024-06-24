4c0c38e49bb93f52a08025a3f631b8baec6daaa4
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-06-24 12:17:11 -0600

    WildOasis - Updating application settings

--- 16-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ 16-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import Input from '../../ui/Input';
 import FormRow from '../../ui/FormRow';
 import Spinner from '../../ui/Spinner';
 import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSettings';
 
 function UpdateSettingsForm() 
 {
@@ -17,36 +18,57 @@ function UpdateSettingsForm()
     } = {},
   } = useSettings();
 
+  const {isUpdating, UpdateSetting} = useUpdateSetting();
+
   if(isLoading) return <Spinner />
+
+  function HandleUpdate(event) 
+  {
+    const { value, id, defaultValue } = event.target;
+    console.log(value);
+ 
+    if (!value || !id || defaultValue === value) return;
+
+    UpdateSetting({ [id]: value })
+    event.target.defaultValue = value;
+  }
 
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
         <Input 
           type='number' 
-          id='min-nights' 
+          id='minBookingLength' 
+          disabled={isUpdating}
           defaultValue={minBookingLength}
+          onBlur={(event) => HandleUpdate(event)}
         />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
         <Input 
           type='number' 
-          id='max-nights'
+          id='maxBookingLength'
+          disabled={isUpdating}
           defaultValue={maxBookingLength}
+          onBlur={(event) => HandleUpdate(event)}
          />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
         <Input 
           type='number' 
-          id='max-guests' 
+          id='maxGuestsPerBooking' 
+          disabled={isUpdating}
           defaultValue={maxGuestsPerBooking}
+          onBlur={(event) => HandleUpdate(event)}
         />
       </FormRow>
       <FormRow label='Breakfast price'>
         <Input 
           type='number' 
-          id='breakfast-price' 
+          id='breakfastPrice'
+          disabled={isUpdating} 
           defaultValue={breakfastPrice}
+          onBlur={(event) => HandleUpdate(event)}
         />
       </FormRow>
     </Form>
